3f0a5a2809cbfcf0f733fb564f8a2c87c1d5e01c
Shane Fontaine <shane6fontaine@gmail.com>
2019-01-02 19:09:09 -0800

    Add ERC20 Pool tests

--- tests/test_uniswap.py
+++ tests/test_uniswap.py
@@ -18,6 +18,7 @@ class TestUniswap(object):
 
     ONE_ETH = 1*10**18
 
+    # ------ Exchange ------------------------------------------------------------------
     def test_get_fee_maker(self, client):
         r = client.get_fee_maker()
         assert r == 0
@@ -26,6 +27,7 @@ class TestUniswap(object):
         r = client.get_fee_taker()
         assert r == 0.003
 
+    # ------ Market --------------------------------------------------------------------
     @pytest.mark.parametrize('token, qty', [
         ('bat', ONE_ETH),
         ('dai', ONE_ETH),
@@ -68,4 +70,29 @@ class TestUniswap(object):
         ])
     def test_get_token_eth_output_price(self, client, token, qty):
         r = client.get_token_eth_output_price(token, qty)
+        assert bool(r)
+
+    # ------ ERC20 Pool ----------------------------------------------------------------
+    @pytest.mark.parametrize('token', [
+        ('bat'),
+        ('dai')
+        ])
+    def test_get_eth_balance(self, client, token,):
+        r = client.get_eth_balance(token)
+        assert bool(r)
+
+    @pytest.mark.parametrize('token', [
+        ('bat'),
+        ('dai')
+        ])
+    def test_get_token_balance(self, client, token,):
+        r = client.get_token_balance(token)
+        assert bool(r)
+
+    @pytest.mark.parametrize('token', [
+        ('bat'),
+        ('dai')
+        ])
+    def get_exchange_rate(self, client, token,):
+        r = client.get_exchange_rate(token)
         assert bool(r)
