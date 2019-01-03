c9946c44a67865edf59dc1e20d475b09d92899dd
Shane Fontaine <shane6fontaine@gmail.com>
2019-01-02 19:10:12 -0800

    Black formatting

--- tests/test_uniswap.py
+++ tests/test_uniswap.py
@@ -6,14 +6,14 @@ import os
 from uniswap.uniswap import UniswapWrapper
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def client():
-    address = os.environ['ETH_ADDRESS']
-    priv_key = os.environ['ETH_PRIV_KEY']
+    address = os.environ["ETH_ADDRESS"]
+    priv_key = os.environ["ETH_PRIV_KEY"]
     return UniswapWrapper(address, priv_key)
 
 
-@pytest.mark.usefixtures('client')
+@pytest.mark.usefixtures("client")
 class TestUniswap(object):
 
     ONE_ETH = 1*10**18
@@ -28,44 +28,44 @@ class TestUniswap(object):
         assert r == 0.003
 
     # ------ Market --------------------------------------------------------------------
-    @pytest.mark.parametrize('token, qty', [
-        ('bat', ONE_ETH),
-        ('dai', ONE_ETH),
-        ('bat', 2 * ONE_ETH),
-        pytest.param('btc', ONE_ETH,
+    @pytest.mark.parametrize("token, qty", [
+        ("bat", ONE_ETH),
+        ("dai", ONE_ETH),
+        ("bat", 2 * ONE_ETH),
+        pytest.param("btc", ONE_ETH,
                      marks=pytest.mark.xfail)
         ])
     def test_get_eth_token_input_price(self, client, token, qty):
         r = client.get_eth_token_input_price(token, qty)
         assert bool(r)
 
-    @pytest.mark.parametrize('token, qty', [
-        ('bat', ONE_ETH),
-        ('dai', ONE_ETH),
-        ('bat', 2 * ONE_ETH),
-        pytest.param('btc', ONE_ETH,
+    @pytest.mark.parametrize("token, qty", [
+        ("bat", ONE_ETH),
+        ("dai", ONE_ETH),
+        ("bat", 2 * ONE_ETH),
+        pytest.param("btc", ONE_ETH,
                      marks=pytest.mark.xfail)
         ])
     def test_get_token_eth_input_price(self, client, token, qty):
         r = client.get_token_eth_input_price(token, qty)
         assert bool(r)
 
-    @pytest.mark.parametrize('token, qty', [
-        ('bat', ONE_ETH),
-        ('dai', ONE_ETH),
-        ('bat', 2 * ONE_ETH),
-        pytest.param('btc', ONE_ETH,
+    @pytest.mark.parametrize("token, qty", [
+        ("bat", ONE_ETH),
+        ("dai", ONE_ETH),
+        ("bat", 2 * ONE_ETH),
+        pytest.param("btc", ONE_ETH,
                      marks=pytest.mark.xfail)
         ])
     def test_get_eth_token_output_price(self, client, token, qty):
         r = client.get_eth_token_output_price(token, qty)
         assert bool(r)
 
-    @pytest.mark.parametrize('token, qty', [
-        ('bat', ONE_ETH),
-        ('dai', ONE_ETH),
-        ('bat', 2 * ONE_ETH),
-        pytest.param('btc', ONE_ETH,
+    @pytest.mark.parametrize("token, qty", [
+        ("bat", ONE_ETH),
+        ("dai", ONE_ETH),
+        ("bat", 2 * ONE_ETH),
+        pytest.param("btc", ONE_ETH,
                      marks=pytest.mark.xfail)
         ])
     def test_get_token_eth_output_price(self, client, token, qty):
@@ -73,25 +73,25 @@ class TestUniswap(object):
         assert bool(r)
 
     # ------ ERC20 Pool ----------------------------------------------------------------
-    @pytest.mark.parametrize('token', [
-        ('bat'),
-        ('dai')
+    @pytest.mark.parametrize("token", [
+        ("bat"),
+        ("dai")
         ])
     def test_get_eth_balance(self, client, token,):
         r = client.get_eth_balance(token)
         assert bool(r)
 
-    @pytest.mark.parametrize('token', [
-        ('bat'),
-        ('dai')
+    @pytest.mark.parametrize("token", [
+        ("bat"),
+        ("dai")
         ])
     def test_get_token_balance(self, client, token,):
         r = client.get_token_balance(token)
         assert bool(r)
 
-    @pytest.mark.parametrize('token', [
-        ('bat'),
-        ('dai')
+    @pytest.mark.parametrize("token", [
+        ("bat"),
+        ("dai")
         ])
     def get_exchange_rate(self, client, token,):
         r = client.get_exchange_rate(token)
