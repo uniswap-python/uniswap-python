b59f6cc5d0690671c8561abf54f92a97ae628980
Yohan K <72107640+liquid-8@users.noreply.github.com>
2021-07-05 14:44:52 +0300

    Update uniswap/uniswap.py
    Co-authored-by: Erik Bjäreholt <erik.bjareholt@gmail.com>

--- uniswap/uniswap.py
+++ uniswap/uniswap.py
@@ -531,6 +531,8 @@ class Uniswap:
         elif self.version == 3:
             if fee_on_transfer:
                 raise Exception('fee on transfer not supported by Uniswap v3')
+            if fee_on_transfer:
+                raise Exception('fee on transfer not supported by Uniswap v3')
             return self._token_to_token_swap_input(
                 input_token, self.get_weth_address(), qty, recipient, fee, slippage
             )
