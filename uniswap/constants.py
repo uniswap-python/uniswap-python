f64bdee497143d85c81649e2e6c7ca05a54bb4b8
liquid-8 <moyovedro@gmail.com>
2023-06-27 22:51:58 +0300

    v4 support pre-alpha

--- uniswap/constants.py
+++ uniswap/constants.py
@@ -70,8 +70,7 @@ _router_contract_addresses_v2 = {
     "harmony_mainnet": "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
     "harmony_testnet": "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
 }
-#
- need to replace with actual addresses
+# need to replace with actual addresses
 _poolmanager_contract_addresses = {
     "mainnet": "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
     "ropsten": "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
