62c4ac413ff78f6085b5435257fdf308a847df90
Erik Bjäreholt <erik@bjareho.lt>
2023-09-09 14:16:56 +0200

    fix: added __all__ to __init__

--- uniswap/__init__.py
+++ uniswap/__init__.py
@@ -1,3 +1,5 @@
 from . import exceptions
-from .uniswap import Uniswap, _str_to_addr
 from .cli import main
+from .uniswap import Uniswap, _str_to_addr
+
+__all__ = ["Uniswap", "exceptions", "_str_to_addr", "main"]
