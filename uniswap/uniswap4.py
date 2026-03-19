01f1814e0d3b431bfebd684cd7dec4478915b6a9
Yohan Key <moyovedro@gmail.com>
2026-03-20 01:48:20 +0300

    tech commit

--- uniswap/uniswap4.py
+++ uniswap/uniswap4.py
@@ -1484,7 +1484,6 @@ class Uniswap4:
             # `(token0, token1, fee)` hasn't been deployed.
             return 1
         price = (
-        price = (
             quote_amount / (qty / (10 ** self.get_token(_str_to_addr(token0)).decimals))
         ) / 10 ** self.get_token(_str_to_addr(token1)).decimals
 
