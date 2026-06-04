1eccd7a794117a9c0d7ff013e71250eebb0c041c
Yohan Key <moyovedro@gmail.com>
2026-06-04 12:58:01 +0300

    fix

--- uniswap/uniswap4.py
+++ uniswap/uniswap4.py
@@ -128,6 +128,8 @@ class Uniswap4:
         self.net_id = chain_id
         if self.net_id in _netid_to_name:
             self.net_name = _netid_to_name[self.net_id]
+            if self.net_name not in _quoter_contract_addresses_v4:
+                raise Exception(f"Netid {self.net_id} is not supported yet.")
         else:
             raise Exception(f"Unknown netid: {self.net_id}")
 
